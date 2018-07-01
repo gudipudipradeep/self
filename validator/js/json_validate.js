5eebfd336049b04de8e45c84f5533a06ad32adb4
gudipudipradeep <deepu.rao90@gmail.com>
2018-07-01 13:39:04 +0530

    Add files via upload

---
+++ validator/js/json_validate.js
@@ -0,0 +1,28 @@
+(function () {
+$( "#json_validate_status" ).hide();
+$('#side_nav').toggle(
+function() {
+  $('#body_node').css('left', '0');
+},
+function() {
+  $('#body_node').css('left', '200px');
+});
+$("#json_btn_validate").click(function(){
+	$( "#json_validate_status" ).hide();
+	try {
+	  	var json_obj = JSON.parse($('textarea#json_validate').val());
+	  	$( "#json_validate_status" ).html( "JSON Validated Successfully!" );
+	  	$( "#json_validate_status" ).show();
+	}
+	catch (err) {
+		$( "#json_validate_status" ).html( err.toString() );
+		$( "#json_validate_status" ).show();
+		alert(err);
+	}
+});
+$("#json_btn_format").click(function(){
+	$( "#json_validate_status" ).hide();
+	var format_obj = JSON.parse($('textarea#json_validate').val());
+	$("textarea#json_validate").val(JSON.stringify(format_obj, null, 4));
+});
+})();
