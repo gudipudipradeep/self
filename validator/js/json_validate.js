5df4fae32a6ea69a6c289c5cde756bf8342879d4
Pradeep Gudipudi <pradeep.gudipudi@infor.com>
2018-07-15 11:45:24 +0530

    Yaml Validator Code changes

--- validator/js/json_validate.js
+++ validator/js/json_validate.js
@@ -25,4 +25,11 @@ $("#json_btn_format").click(function(){
 	var format_obj = JSON.parse($('textarea#json_validate').val());
 	$("textarea#json_validate").val(JSON.stringify(format_obj, null, 4));
 });
+$("#yaml_textarea").click(function(){
+	$("#validator_name").html( "Validate YAML" );
+});
+$("#json_textarea").click(function(){
+	$("#validator_name").html( "Validate Json" );
+});
+
 })();
