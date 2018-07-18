0322c16b34ea7b77f7ab035f0c8fe05205062beb
Pradeep Gudipudi <pradeep.gudipudi@infor.com>
2018-07-18 23:34:10 +0530

    File uploading javascript code changes

--- validator/js/json_validate.js
+++ validator/js/json_validate.js
@@ -1,4 +1,48 @@
 (function () {
+
+// File Upload Code Changes
+	var input = document.getElementById('files');
+	var single_file = document.getElementById('single_file');
+	
+	var table = document.getElementById("file_content_details");
+	//folder upload
+	input.onchange = function(e) {
+	  var files = e.target.files; // FileList
+	    for (var i = 0, f; f = files[i]; ++i){
+	        console.debug(files[i].webkitRelativePath);
+	        var row = table.insertRow(-1);
+	        var cell1 = row.insertCell(0);
+	        var cell2 = row.insertCell(1);
+
+	        // Row With Data
+	        cell1.innerHTML = files[i].webkitRelativePath;
+	        cell2.innerHTML = files[i].size
+	        
+	    }
+	}
+	//single file upload
+	single_file.onchange = function(e) {
+		var files = e.target.files; // FileList
+	    for (var i = 0, f; f = files[i]; ++i){
+	    	console.debug(files[i].name);
+	        var row = table.insertRow(-1);
+	        var cell1 = row.insertCell(0);
+	        var cell2 = row.insertCell(1);
+
+	        // Row With Data
+	        cell1.innerHTML = files[i].name;
+	        cell2.innerHTML = files[i].size
+	    }
+	}
+	
+var control = $("#control");
+
+$("#clear").click(function () {
+	$("#file_content_details").find("tr:not(:first)").remove();
+	$('#files').val("");
+});
+
+//
 $( "#json_validate_status" ).hide();
 $('#side_nav').toggle(
 function() {
