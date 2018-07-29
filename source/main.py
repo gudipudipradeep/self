5aa642421d38f79e279242a32850b687dec30fd5
Pradeep Gudipudi <pradeep.gudipudi@infor.com>
2018-07-29 11:40:55 +0530

    logging code changes

--- source/main.py
+++ source/main.py
@@ -1,8 +1,39 @@
-from bottle import hook, route, response, run, default_app, request
+from bottle import hook, route, response, run, default_app, request, Bottle
 from upload import file_upload
 import sys
 from bottle import static_file
 import os
+from datetime import datetime
+from functools import wraps
+import logging
+
+logger = logging.getLogger()
+
+# set up the logger
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('myapp.log')
+formatter = logging.Formatter('%(msg)s')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+def log_to_logger(fn):
+    '''
+    Wrap a Bottle request so that a log line is emitted after it's handled.
+    (This decorator can be extended to take the desired logger as a param.)
+    '''
+    @wraps(fn)
+    def _log_to_logger(*args, **kwargs):
+        request_time = datetime.now()
+        actual_response = fn(*args, **kwargs)
+        # modify this to log exactly what you need:
+        logger.info('%s %s %s %s %s' % (request.remote_addr,
+                                        request_time,
+                                        request.method,
+                                        request.url,
+                                        response.status))
+        return actual_response
+    return _log_to_logger
 
 app = application = default_app()
  
@@ -17,9 +48,12 @@ _allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
 if not os.path.exists(os.path.dirname(download_path)):
     os.makedirs(os.path.dirname(download_path))
     
+#adding global variables
+app.install(log_to_logger)
 app.config['file_save_path']    = download_path 
 app.config['web_files']    = dir_path
- 
+
+
  
 @route('/<filepath:path>')
 def server_static(filepath):
---
+++ source/snippet.py
@@ -0,0 +1,26 @@
+import bottle
+from bottle import route, run
+
+@route('/index')
+def index():
+    return '<h1>Hello Bottle!</h1>'
+
+
+app = bottle.default_app()
+
+# import wsgiserver
+# from cgi import parse_qs, escape
+# 
+# def hello_world(environ, start_response):
+#     parameters = parse_qs(environ.get('QUERY_STRING', ''))
+#     if 'subject' in parameters:
+#         subject = escape(parameters['subject'][0])
+#     else:
+#         subject = 'World'
+#     start_response('200 OK', [('Content-Type', 'text/html')])
+#     return ['''Hello %(subject)s Hello %(subject)s!''']
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8090, app)
+    srv.serve_forever()
