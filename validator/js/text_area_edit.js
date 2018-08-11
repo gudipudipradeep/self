6b0229912d4bc6c014414a5ff4183d299cb3526d
Pradeep Gudipudi <pradeep.gudipudi@infor.com>
2018-08-11 18:04:03 +0530

    Updated solution table for the issues

--- validator/js/text_area_edit.js
+++ validator/js/text_area_edit.js
@@ -1,24 +1,15 @@
 (function () {
-	$("#chose_container").hide();
 	$("#pencil").click(function (e) {
-		e.preventDefault();
-		$("#chose_container").show();
-		$("#main_container").show();
-		$("#chose_container").css({"border":"1px solid black"});
-		$("#main_container").css({"border":"1px solid black"});
-		$("#main_container").attr("contenteditable","true");
+		$("#edit_tools").show();
+		$(".note-editable").attr("contenteditable","true");
 		
+
 	});
+	
 	$("#floppy").click(function () {
-		$("#main_container").attr("contenteditable","false");
-		$("#chose_container").hide();
-		$("#chose_container").css({"border":""});
-		$("#main_container").css({"border":""});
-		$("#main_container").attr("contenteditable","false");
-		var edited_text = $( "#main_container" ).html();
-		alert(edited_text);
-		edited_text = edited_text.replace(/<div>/g, "<p>").replace("</div>", "</p>").replace(/<br>/g, "</p>");
-		$( "#main_container").empty();
-		$( "#main_container").append(edited_text);		
+		$("#edit_tools").hide();
+		$(".note-editable").attr("contenteditable","flase");
 	});
+	$("#edit_tools").hide();
+	$(".note-editable").attr("contenteditable","flase");	
 })();
